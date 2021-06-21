eb092d372525d18e2b76288a84b49e7975b8d87a
TTcUxJIoP <aiyufanov@miem.hse.ru>
2021-06-21 21:09:38 +0300

    clear

--- CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/miron/opencv
-# Build directory: /home/miron/opencv
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
