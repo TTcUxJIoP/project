874766f7f06fe8a64c53a37f3d53f34150239109
TTcUxJIoP <aiyufanov@miem.hse.ru>
2021-06-21 20:24:00 +0300

    clear

--- proj_test_include-b858cb2.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/miron/opencv/proj_test_tests-b858cb2.cmake")
-  include("/home/miron/opencv/proj_test_tests-b858cb2.cmake")
-else()
-  add_test(proj_test_NOT_BUILT-b858cb2 proj_test_NOT_BUILT-b858cb2)
-endif()
