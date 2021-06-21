6161894aa1848c1d5e7d4ccedfb1b0c88b3e3a92
TTcUxJIoP <aiyufanov@miem.hse.ru>
2021-06-21 21:11:10 +0300

    clear

--- build/doctest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/miron/opencv/doctest
-# Build directory: /home/miron/opencv/build/doctest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
